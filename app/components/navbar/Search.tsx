301a6ea5f8b832c641da4873328dc98ab9d1bc00
Lola <78376398+lolaedun@users.noreply.github.com>
2024-08-28 19:58:25 +0100

    add bi search react icon and add guests to nav bar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import {BiSearch} from 'react-icons/bi'
 
 const Search = () => {
 
@@ -10,6 +11,15 @@ const Search = () => {
                 </div>
                 <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
                     Any Week
+                </div>
+                <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+                    <div className="hidden sm:block">
+                        Add Guests
+                    </div>
+                    <div className="p-2 bg-rose-500 rounded-full text-white">
+                        <BiSearch size={18} />
+
+                    </div>
                 </div>
             </div>
         </div>
