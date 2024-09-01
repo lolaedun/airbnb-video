18e3c68a6645aedeb523cfdae4a5af173efe5a5c
Lola <78376398+lolaedun@users.noreply.github.com>
2024-09-01 10:51:35 +0100

    fix page reload error message

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly"
 
 const font = Nunito({
    subsets: ["latin"] 
   });
 
 export const metadata: Metadata = {
-  title: "Airbnb",
+  title: "Lolas Home",
   description: "Airbnb clone by Lola Edun",
 };
 
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
