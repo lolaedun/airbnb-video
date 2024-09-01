f757b72c48101f1c8a696546b2c66faf1e39a5c2
Lola <78376398+lolaedun@users.noreply.github.com>
2024-09-01 12:20:53 +0100

    set up initial modal for submit and timeout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly"
+import Modal from "./components/modals/modal"
 
 const font = Nunito({
    subsets: ["latin"] 
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal />
           <Navbar />
         </ClientOnly>
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-   <div className="text-rose-500 text-2xl">Hello Lola this is Airbnb!</div>
+   <div className="text-rose-500 text-2xl">Hello Lola this is Lolas Home bnb!</div>
   );
 }
