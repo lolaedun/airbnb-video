9f83d5cb641713f248d90ef2f7ac27ddd3bac814
Lola <78376398+lolaedun@users.noreply.github.com>
2024-09-03 16:00:54 +0100

    add test login modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen/>
+          <Modal isOpen  />
           <Navbar />
         </ClientOnly>
         {children}
