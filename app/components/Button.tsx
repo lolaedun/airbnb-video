8ad2b3df1f9636f39170f5f9e5556ed1fcb6fef8
Lola <78376398+lolaedun@users.noreply.github.com>
2024-09-04 10:50:40 +0100

    style outline and small buttons

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -34,7 +34,10 @@ const Button:React.FC<ButtonProps> = ({
         ${outline ? 'bg-white' : 'bg-sky-500'}
         ${outline ?  'border-black' : 'border-sky-500'}
         ${outline ? 'text-black' : 'text-white'}
-        
+        ${small? 'py-1' : 'py-3'}
+        ${small? 'text-sm' : 'text-md'}
+        ${small? 'font-light' : 'font-semibold'}
+        ${small? 'border-[1px]' : 'border-2'}
         `}>
             {label}
         </button>
