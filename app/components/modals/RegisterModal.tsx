32978431e486e0e6541a07d699be1df627443c56
Lola <78376398+lolaedun@users.noreply.github.com>
2024-09-06 11:13:28 +0100

    create input box for registar modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,7 +8,8 @@ import { useCallback, useState } from "react";
 import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
-import Heading from "../Heading"
+import Heading from "../Heading";
+import Input from "../inputs/Input";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -53,6 +54,14 @@ const bodyContent = (
     <Heading 
       title="Welcome to Lola's Home"
       subtitle="Create an account!"
+    />
+    <Input 
+      id="email"
+      label="Email"
+      disabled={isLoading}
+      register={register}
+      errors={errors}
+      required
     />
   </div>
 )
