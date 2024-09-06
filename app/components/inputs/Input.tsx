25141103a2aed4993f54d10ed069fa5647e73a5c
Lola <78376398+lolaedun@users.noreply.github.com>
2024-09-06 14:19:28 +0100

    animate email input on signup

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,46 +1,49 @@
 'use client';
 
 import { text } from 'node:stream/consumers';
-import React from 'react'
+import React from 'react';
 import {UseFormRegister,
         FieldValues,
         FieldErrors} from 'react-hook-form';
 
 import {BiDollar} from 'react-icons/bi';
 
-interface InputProps{
-    id: string;
-    label: string;
-    type?: string;
-    disabled?: boolean;
-    formatPrice?: boolean;
-    required?:boolean;
-    register: UseFormRegister<FieldValues>;
-    errors: FieldErrors;
-
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>,
+  errors: FieldErrors
 }
 
 
 
-const Input:React.FC<InputProps> = ({
-
-    id,
-    label,
-    type = 'text',
-    disabled,
-    formatPrice,
-    required,
-    register,
-    errors
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type = "text", 
+  disabled, 
+  formatPrice,
+  register,
+  required,
+  errors,
 }) => {
   return (
-    <div className='w-full relative'>
-        {formatPrice && (
-            <BiDollar 
-            size= {24}
-            className ='text-neutral-700 absolute top-5 left-2'
-            />
-        )}
+    <div className="w-full relative">
+      {formatPrice && (
+        <BiDollar
+          size={24}  
+          className="
+            text-neutral-700
+            absolute
+            top-5
+            left-2
+          "
+        />
+      )}
         <input 
             id={id}
             disabled={disabled}
@@ -63,14 +66,31 @@ const Input:React.FC<InputProps> = ({
             ${formatPrice ? 'pl-9': 'pl-4'}
             ${errors [id]? 'border-sky-500': 'border-neutral-300'}
             ${errors [id]? 'focus:border-sky-500': 'focus:border-black'}
-            
-            
-            
-            `}
+             `}
         />
+        <label className={`
+            absolute
+            text-md
+            duration-150
+            transform
+            -translate-y-3
+            top-5
+            z-10
+            origin-[0]
+            ${formatPrice ? 'left-9':'left-4'}
+            peer-placeholder-shown:scale-100
+            peer-placeholder-shown:translate-y-0
+            peer-focus:scale-75
+            peer-focus:-translate-y-4
+            ${errors[id] ? 'text-sky-500' : 'text-zinc-400'}
+        
+        `}
+        >
+            {label}
+        </label>
 
     </div>
-  )
+  );
 }
 
-export default Input
+export default Input;
