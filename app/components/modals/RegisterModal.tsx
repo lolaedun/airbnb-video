28ad2cfddb0585c55a78bbf6a328c080ef43a2f7
Lola <78376398+lolaedun@users.noreply.github.com>
2024-09-09 10:16:57 +0100

    Add login and close on click to register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -101,6 +101,17 @@ const footerContent=(
         icon={AiFillGithub}
         onClick={() => {}}
       />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>
+            Already have an account?
+          </div>
+          <div onClick={registerModal.onClose}
+          className="text-neutral-800 cursor-pointer hover:underline">
+            Login
+          </div>
+        </div>
+      </div>
 
   </div>
 )
