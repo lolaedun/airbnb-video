ff05302e3a4b53650c3ef6782bb4df7647a13c2b
Lola <78376398+lolaedun@users.noreply.github.com>
2024-09-19 10:40:39 +0100

    add first step to rent modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,23 +1,70 @@
 'use client';
 import React from 'react';
+import { useMemo, useState } from "react";
 import useRentModal from '@/app/hooks/useRentModal';
-
 import Modal from "./Modal";
+
+import Heading from '../Heading';
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5,
+  }
 
 const RentModal = () => {
 
     const rentModal = useRentModal();
-  return (
-    <Modal
+    const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const onBack = () => {
+        setStep((value) => value - 1);
+    }
     
-    isOpen={rentModal.isOpen}
-    onClose={rentModal.onClose}
-    title="Choose your home!"
-    actionLabel="Submit"
-    onSubmit={rentModal.onClose}
+    const onNext = () => {
+    setStep((value) => value + 1);
+    }
+    const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+        return 'Create'
+        }
     
-  />
-  )
+        return 'Next'
+    }, [step]);
+
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.CATEGORY) {
+        return undefined
+        }
+
+        return 'Back'
+    }, [step]);
+
+    let bodyContent = (
+        <div className="flex flex-col gap-8">
+          <Heading
+            title="Which of these best describes your place?"
+            subtitle="Pick a category"/>
+        </div>
+    )
+
+    return (
+            <Modal
+            
+            isOpen={rentModal.isOpen}
+            onClose={rentModal.onClose}
+            title="Choose your home!"
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+            onSubmit={rentModal.onClose}
+            body={bodyContent}
+            
+            />
+        )
 }
 
 export default RentModal;
